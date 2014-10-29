05b9f9e56d110c7071ffa371c197bd3dcddd324e
a45154630 <a45154630@163.com>
2014-10-29 12:04:13 +0800

    add new parse function

--- experiments/disk-io-cache/disk-io-parse.py
+++ experiments/disk-io-cache/disk-io-parse.py
@@ -39,10 +39,54 @@ def parseFile(prefix, desFile):
 		writer.writerow(l1 + l2[1:] + l3[1:] + l4[1:])
 
 
+def parseFile2(desFile):
+	writer = csv.writer(desFile)
+
+	writer.writerow(['####','####','SSD/HDD','####','####'])
+	writer.writerow(['RAND_READ','RAND_WRITE','SEQ_READ','SEQ_WRITE'])
+
+	rr1 = readFile('ran-read-ssd')
+	rr2 = readFile('ran-read-hdd')
+	
+	rw1 = readFile('ran-write-ssd')
+	rw2 = readFile('ran-write-hdd')
+
+	sr1 = readFile('seq-read-ssd')
+	sr2 = readFile('seq-read-hdd')
+
+	sw1 = readFile('seq-write-ssd')
+	sw2 = readFile('seq-write-hdd')
+
+	for i in range(len(rr1)):
+		line = []
+
+		rrl1 = parseLine(rr1[i])
+		rrl2 = parseLine(rr2[i])
+		tmp = float(rrl1[2]) / float(rrl2[2])
+		line.append(str(tmp))
+
+		rwl1 = parseLine(rw1[i])
+		rwl2 = parseLine(rw2[i])
+		tmp = float(rwl1[2]) / float(rwl2[2])
+		line.append(str(tmp))
+
+		srl1 = parseLine(sr1[i])
+		srl2 = parseLine(sr2[i])
+		tmp = float(srl1[2]) / float(srl2[2])
+		line.append(str(tmp))
+
+		swl1 = parseLine(sw1[i])
+		swl2 = parseLine(sw2[i])
+		tmp = float(swl1[2]) / float(swl2[2])
+		line.append(str(tmp))
+
+		writer.writerow(line)
+
 ### main
 f = file('result.csv', 'wb')
 parseFile('read', f)
 parseFile('write', f)
+parseFile2(f)
 f.close()
 
 
